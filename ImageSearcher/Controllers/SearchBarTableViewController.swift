a9a79c06b441086e19836c9c3dea82165f417485
Randy McLain <Randoramma@me.com>
2017-10-11 07:35:24 -0700

    Having issues with parsing the JSON after adding in pagination parameters in subsequent page loadings after the first.

--- ImageSearcher/Controllers/SearchBarTableViewController.swift
+++ ImageSearcher/Controllers/SearchBarTableViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class SearchBarTableViewController: UITableViewController, UISearchBarDelegate {
     let reuseIdentifier:String = "tableCell"
     var searchBarController: UISearchController! // will not be nil
- 
+    var didMakeRequest: Bool! // ensures only 1 request is made per user scroll action.
+    
     var dataSourceOfPhotos = [Photo]() {
         didSet {
             self.tableView.reloadData()
@@ -23,30 +24,25 @@ class SearchBarTableViewController: UITableViewController, UISearchBarDelegate {
             self.tableView.reloadData()
         }
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBarControllerSetup()
-
-        RequestManager.sharedInstance.getEditorChoiceImages {[weak self] (photos, error) in
-            if error == nil {
-                self?.dataSourceOfPhotos = photos!
-            }
-        }
+        self.didMakeRequest = false
+        requestDefaultPhotos()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return self.searchBarController.isActive ? self.dataSourceOfSearch.count : self.dataSourceOfPhotos.count
@@ -62,53 +58,11 @@ class SearchBarTableViewController: UITableViewController, UISearchBarDelegate {
         }
         return cell
     }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
     
     // MARK: SearchBar Delegate
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        let textToFind = searchBar.text!
-        
-        RequestManager.sharedInstance.getImagesByName(nameToSearch: textToFind) { (photos, error) in
-            if error == nil {
-                self.dataSourceOfSearch = photos!
-            }
-        }
+        requestSearchedForPhotos(searchBar.text!)
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
@@ -118,9 +72,33 @@ class SearchBarTableViewController: UITableViewController, UISearchBarDelegate {
     func updateSearchResultsForSearchController(searchController: UISearchController) {
         
     }
-
+    
+    // MARK: - ScrollView
+    
+    /*
+     Notifies when the user scrolls, when the reach the bottom so we can know when to load new requests
+     Note: scrollViewDidScroll may be called several times during a scroll event and thus the need for the
+     didMakeRequest variable.
+     */
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        
+        if offsetY > contentHeight - scrollView.frame.size.height {
+            if !self.didMakeRequest {
+                self.didMakeRequest = true
+                if (self.searchBarController.isActive) && (self.searchBarController.searchBar.text != "") {
+                    requestSearchedForPhotos(self.searchBarController.searchBar.text!)
+                    self.didMakeRequest = false
+                }
+            } else {
+                requestDefaultPhotos()
+            }
+        }
+    }
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destinationVC = segue.destination as! DetailViewController
@@ -133,7 +111,7 @@ class SearchBarTableViewController: UITableViewController, UISearchBarDelegate {
         /* Setup the detail view with DI */
         destinationVC.detailVCSetup(withImageView: cell.mainImage.image!, likesCount: self.dataSourceOfPhotos[indexOfCellSelected].likes!, andFavoritesCount: self.dataSourceOfPhotos[indexOfCellSelected].favorites!)
     }
-
+    
     // MARK: - Helper
     
     fileprivate func searchBarControllerSetup() {
@@ -148,5 +126,20 @@ class SearchBarTableViewController: UITableViewController, UISearchBarDelegate {
         
         self.searchBarController.searchBar.text = "Search for Images"
     }
-   
+    
+    fileprivate func requestSearchedForPhotos(_ textToFind: String) {
+        RequestManager.sharedInstance.getImagesByName(nameToSearch: textToFind) { (photos, error) in
+            if error == nil {
+                self.dataSourceOfSearch += photos!
+            }
+        }
+    }
+    
+    fileprivate func requestDefaultPhotos() {
+        RequestManager.sharedInstance.getEditorChoiceImages {[weak self] (photos, error) in
+            if error == nil {
+                self?.dataSourceOfPhotos += photos!
+            }
+        }
+    }
 }
--- ImageSearcher/Models/RequestManager.swift
+++ ImageSearcher/Models/RequestManager.swift
@@ -19,6 +19,8 @@ import SwiftyJSON
  */
 class RequestManager {
     static let sharedInstance = RequestManager()
+    private var pageForSearch: Int
+    private var pageForEditorsChoice: Int
     
     private struct API {
         
@@ -31,16 +33,24 @@ class RequestManager {
     
     // private initalizer
     private init() {
-        
+        self.pageForEditorsChoice = 1;
+        self.pageForSearch = 1;
     }
     
     /// Alamofire Service Methods
     /// see https://github.com/Alamofire/Alamofire#parameter-encoding for options on request parameters.
     func getImagesByName(nameToSearch name: String, completionHandler: @escaping (_ photos: [Photo]?, _ error: Error?) -> Void) {
-        let parameters: [String: Any] = ["key": API.apiKey, "q": name]
+        let queryString = name.URLEncodedValue
+        let parameters: [String: Any] = ["key": API.apiKey,
+                                         "q": queryString,
+                                         "page": pageForSearch,
+                                         "per_page": 10]
+        // perform request and increment if request succeeds.
+
         Alamofire.request(API.baseURL, parameters: parameters).responseJSON { (response) in
             switch(response.result) {
             case .success(let value):
+                self.pageForSearch += 1
                 let json = JSON(value)
                 let photos = self.parseJsonForPhoto(json) // parse for objects
                 DispatchQueue.main.async(execute: {
@@ -56,7 +66,8 @@ class RequestManager {
     }
     
     func getImagesByID(imageID id: String) {
-        let parameters: [String: Any] = ["key": API.apiKey, "id": id]
+        let idQueryString = id.URLEncodedValue
+        let parameters: [String: Any] = ["key": API.apiKey, "id": idQueryString]
         
         Alamofire.request(API.baseURL, parameters: parameters).responseJSON { (response) in
             switch(response.result) {
@@ -68,8 +79,10 @@ class RequestManager {
         }
     }
     
+    /*
+     Automagically caches each image recieved from network requests to improve responsiveness.  Prior to making network request AlamofireImage checks if the image was requested  previously and if so pulls that image from the cache.  Only works while connecetd to network.
+     */
     func getImageByURL(urlOfImage url: String, completionHandler: @escaping (_ image: UIImage?, _ error: Error?) ->Void) {
-        
         Alamofire.request(url).responseImage { (response) in
             switch(response.result) {
             case .success(let value):
@@ -81,11 +94,15 @@ class RequestManager {
     }
     
     func getEditorChoiceImages(completionHandler: @escaping (_ photos: [Photo]?, _ error: Error?) -> Void) {
-        let parameters: [String: Any] = ["key": API.apiKey, "editors_choice": true]
-        // the fetch request
+        let parameters: [String: Any] = ["key": API.apiKey,
+                                         "editors_choice": true,
+                                         "page": pageForEditorsChoice,
+                                         "per_page": 10]
+        // the fetch request  and increment request variable if request succeeds.
         Alamofire.request(API.baseURL, parameters: parameters).responseJSON { (response) in
             switch(response.result) {
             case .success(let value):
+                self.pageForEditorsChoice += 1
                 let json = JSON(value)
                 let photos = self.parseJsonForPhoto(json) // parse for objects
                 DispatchQueue.main.async(execute: {
@@ -116,6 +133,41 @@ class RequestManager {
         return photoArray
     }
 }
+
+public extension String {
+    
+    /* For performance, I've replaced the char constants with integers, as char constants don't work in Swift.
+    https://stackoverflow.com/questions/3423545/objective-c-iphone-percent-encode-a-string/20271177#20271177
+ 
+     */
+    var URLEncodedValue: String {
+        let output = NSMutableString()
+        guard let source = self.cString(using: String.Encoding.utf8) else {
+            return self
+        }
+        let sourceLen = source.count
+        
+        var i = 0
+        while i < sourceLen - 1 {
+            let thisChar = source[i]
+            if thisChar == 32 {
+                output.append("+")
+            } else if thisChar == 46 || thisChar == 45 || thisChar == 95 || thisChar == 126 ||
+                (thisChar >= 97 && thisChar <= 122) ||
+                (thisChar >= 65 && thisChar <= 90) ||
+                (thisChar >= 48 && thisChar <= 57) {
+                output.appendFormat("%c", thisChar)
+            } else {
+                output.appendFormat("%%%02X", thisChar)
+            }
+            
+            i += 1
+        }
+        
+        return output as String
+    }
+}
+
 
 /*
  hits =     (
